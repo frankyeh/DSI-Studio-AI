6616cc6ac79c4062b2963ef1c1aad703ab4df2d9
Fang-Cheng (Frank) Yeh <frank.yeh@gmail.com>
2026-07-29 20:03:39 -0400

    Simplify dsi.cmd syntax

--- dsi.cmd
+++ dsi.cmd
@@ -1,1 +1,1 @@
-@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0dsi_agent.ps1" "%DSI_STUDIO_AGENT%" "%CODEX_THREAD_ID%" %*
+@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0dsi_agent.ps1" %*
