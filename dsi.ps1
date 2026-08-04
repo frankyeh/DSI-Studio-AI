78d9ab5dac4a9520294eb9c1991660eb61319264
Yeh <frank.yeh@gmail.com>
2026-08-03 20:18:59 -0400

    Update dsi.ps1

--- dsi.ps1
+++ dsi.ps1
@@ -1,8 +1,5 @@
 param(
-    [Parameter(Mandatory,Position=0)]
-    [string]$Target,
-
-    [Parameter(Position=1,ValueFromRemainingArguments)]
+    [Parameter(Position=0,ValueFromRemainingArguments)]
     [string[]]$Value,
 
     [string]$Chat
@@ -33,23 +30,16 @@ function Convert-DsiValue([string]$Text)
     return $Text
 }
 
+if(!$Value.Count -and !$Chat) { throw 'Missing command name.' }
+
 $request = [ordered]@{agent=$Agent;session=$Session}
-switch($Target.ToUpperInvariant())
+if($Value.Count)
 {
-    'LIST'  {$request.request = 'LIST'}
-    'LOG'   {$request.request = 'LOG'}
-    'CHAT'  {$request.request = 'CHAT';  $request.chat = $Value -join ' '}
-    'TITLE' {$request.request = 'TITLE'; $request.title = $Value -join ' '}
-    default
-    {
-        if(!$Value.Count) { throw 'Missing command name.' }
-        $request.request = 'CMD'
-        $request.window = $Target
-        $request.command = [ordered]@{cmd=$Value[0]}
-        $param = @($Value | Select-Object -Skip 1 | ForEach-Object {Convert-DsiValue $_})
-        if($param.Count -eq 1) { $request.command.param = $param[0] }
-        elseif($param.Count -gt 1) { $request.command.param = $param }
-    }
+    $cmd = [ordered]@{cmd=$Value[0]}
+    $param = @($Value | Select-Object -Skip 1 | ForEach-Object {Convert-DsiValue $_})
+    if($param.Count -eq 1) { $cmd.param = $param[0] }
+    elseif($param.Count -gt 1) { $cmd.param = $param }
+    $request.command = $cmd
 }
 if($Chat) { $request.chat = $Chat }
 
